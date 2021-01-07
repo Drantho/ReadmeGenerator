c07b1bd18ae11d10d7521039e0b4ec6f3a7ef9e5
Drantho <drantho@gmail.com>
2021-01-06 22:02:05 -0800

    index functions updated

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# ReadmeGenerator
-Node.js interactive readme generator
+# Readme generator 
+
+  
+
+  -------------------------------------------------------------------------------
+
+  
+
+  ## Description
+
+  This is an automatic readme generator. Program will ask user for questions about sections that should be in readme files(e.g. Project name, description, contact, installation...) Readme will then be generated with markdown included.
+
+  -------------------------------------------------------------------------------
+
+  
+
+  ## Installation 
+
+  
+
+  None necessary. Node.js must be installed.
+
+  -------------------------------------------------------------------------------
+
+  
+
+  ## Usage 
+
+  
+
+  Open terminal, navigate to directory, run "node index.js" 
+
+  -------------------------------------------------------------------------------
+
+  
+
+  ## Contributing 
+
+  
+
+  Anthony Mitchell
+
+  -------------------------------------------------------------------------------
+
+  
+
+  ## Tests 
+
+  none 
+
+  -------------------------------------------------------------------------------
+
+  
+
+  ## Questions
+  Email me at drantho@gmail.com with any questions. 
+
+  -------------------------------------------------------------------------------
+
+  
+
+  ## License
+  !(./MIT.png?raw=true) MIT 
+
+  www.MIT.com
--- index.js
+++ index.js
@@ -12,10 +12,53 @@ const rl = readline.createInterface({
 const questions = ["Project name: ", "Github username: ", "Email: ", "Description: ", "Installation: ", "Usage: ", "License: ", "Contributing: ", "Tests: "];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile(fileName, generateMarkdown(data), (err, file) => {
+        if (err) console.log(err)
+
+        console.log("file created successfuly");
+    });
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+    const data = {};
+
+    //TODO Convert to work from list
+    rl.question(questions[0], (name) =>{
+        rl.question(questions[1], (username) => {
+            rl.question(questions[2], (email) => {
+                rl.question(questions[3], (description) => {
+                    rl.question(questions[4], (installation) => {
+                        rl.question(questions[5], (usage) => {
+                            rl.question(questions[6], (license) => {
+                                rl.question(questions[7], (contributing) => {
+                                    rl.question(questions[8], (tests) => {
+                                        data.title = name;
+                                        data.username = username;
+                                        data.email = email;
+                                        data.description = description;
+                                        data.installation = installation;
+                                        data.usage = usage;
+                                        data.license = license;
+                                        data.contributing = contributing;
+                                        data.tests = tests;
+
+                                        writeToFile("readme.md", data);
+                                        rl.close();
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+
+}
 
 // Function call to initialize app
 init();
