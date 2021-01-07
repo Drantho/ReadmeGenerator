df64b01c8687d4264d18b6f977c485ea7460134d
Drantho <drantho@gmail.com>
2021-01-06 21:05:57 -0800

    required libs linked

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // TODO: Include packages needed for this application
+const readline = require("readline");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 // TODO: Create an array of questions for user input
 const questions = [];
